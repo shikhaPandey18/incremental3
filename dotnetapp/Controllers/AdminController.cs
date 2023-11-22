c3b896a49f7e7f2b767d13d8025a44ea3334450f
Iamneo <techadmin@iamneo.ai>
2023-11-22 16:42:04 +0000

    Backup Commit

--- dotnetapp/Controllers/AdminController.cs
+++ dotnetapp/Controllers/AdminController.cs
@@ -99,7 +99,7 @@ namespace dotnetapp.Controllers
         }
         [HttpPut]
         [Route("EditTeam/{id}")]
-        public IActionResult Put(int id, Team Team)
+        public IActionResult PutPlayer(int id, Team Team)
         {
             if(ModelState.IsValid)
             {
@@ -116,7 +116,7 @@ namespace dotnetapp.Controllers
         }
         [HttpDelete]
         [Route("DeleteTeam/{id}")]
-        public IActionResult Delete(int id)
+        public IActionResult DeletePlayer(int id)
         {
   
                 var data=context.Teams.Find(id);
@@ -126,73 +126,7 @@ namespace dotnetapp.Controllers
  
            
         }
-
-
-        // ------------Player Controller---------------------
-
-        [HttpPost]
-        [Route("AddPlayer")]
-        public IActionResult Post(Player Player)
-        {
-            if(ModelState.IsValid)
-            {
-                try{
-                    context.Players.Add(Player);
-                    context.SaveChanges();
- 
-                }
-                catch(System.Exception ex){
-                    return BadRequest(ex.InnerException.Message);
- 
-                }
-            }
-            return Created("Record Added", Player);
- 
-        }
-
-        [HttpPut]
-        [Route("EditPlayer/{id}")]
-        public IActionResult PutPlayer(int id, Player Player)
-        {
-            if(ModelState.IsValid)
-            {
-                Player mv = context.Players.Find(id);
-                mv.Name = Player.Name;
-                mv.Age = Player.Age;
-                mv.Category = Player.Category;
-                mv.BiddingPrice = Player.BiddingPrice;
-                context.SaveChanges();
-                return Ok();
-               
- 
- 
- 
-            }
-            return BadRequest("Unable to Edit Record");
-        }
-
-        [HttpDelete]
-        [Route("DeletePlayer/{id}")]
-        public IActionResult DeletePlayer(int id)
-        {
-  
-                var data=context.Players.Find(id);
-                context.Players.Remove(data);
-                context.SaveChanges();
-                return Ok();
- 
-           
-        }
-
-        [HttpGet]
-        [Route("DisplayTeamPlayers/(id)")]
- 
-        public IActionResult GetMovieDetail(int id)
-        {
-            var data = from d in context.Players where d.TeamId == id select new{
-                Name = d.Name, Age = d.Age, Category = d.Category, BiddingPrice = d.BiddingPrice};
-                return Ok(data);
-        }
-
     }
 }
+
+
